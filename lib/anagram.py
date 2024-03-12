4d3fbe7bbbdc6c9a96b4785edce4e3c7561a5455
VictorGithige <muhuthiagithige1@gmail.com>
2024-03-12 11:14:34 +0300

    final commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [word for word in word_list if self._is_anagram(word)]
+
+    def _is_anagram(self, other_word):
+        other_word_lower = other_word.lower()
+        return sorted(self.word) == sorted(other_word_lower) and self.word != other_word_lower
